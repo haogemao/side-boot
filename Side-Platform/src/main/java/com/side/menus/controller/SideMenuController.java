19bfc16cdc7b6a3133283b360c565402e86c26e4
gmc <gmc@192.168.28.250>
2018-11-08 17:10:26 +0800

    菜单功能控制

--- Side-Platform/src/main/java/com/side/menus/controller/SideMenuController.java
+++ Side-Platform/src/main/java/com/side/menus/controller/SideMenuController.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
