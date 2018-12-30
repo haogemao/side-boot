36f56084318056f08e348220c04f3c3ddea36239
gmc <gmc@192.168.0.126>
2018-12-30 17:01:02 +0800

    增加http请求拦截

--- Side-web/WebContent/js/common/common.js
+++ Side-web/WebContent/js/common/common.js
@@ -1,9 +1,47 @@
 var token=null;
-$.ajaxSetup({
-//拦截所有ajax请求
-    beforeSend: function(xhr) {
-    
-    	var token_url = "/oauth/token";
+//$.ajaxSetup({
+////拦截所有ajax请求
+//    beforeSend: function(xhr) {
+//    
+//    	var token_url = "/oauth/token";
+//    	var authoriz_url = "/oauth/authorize";
+//    	var client_id="client";
+//    	var secret="secret";
+//    	var grant_type = "password";
+//    	
+//        var params = arguments[1].data;
+//        var url = arguments[1].url;
+//        var data = '';
+//        var datas = new Array();
+//         
+//         if(url.search(token_url) > 0){
+//        	 arguments[1].data = params
+//        	 					+ "&client_id=" + client_id 
+//        	 					+ "&client_secret=" + secret 
+//        	 					+ "&grant_type=" + grant_type;
+//        	 
+//         } else {
+//        	 var token = window.sessionStorage.getItem('access_token');
+//        	 xhr.setRequestHeader("Authorization", "Bearer " + token);
+//        	 
+//         }
+//     }
+//});
+
+const ah=require("ajax-hook")
+ah.hookAjax({
+	  //hook callbacks
+	  onreadystatechange:function(xhr){
+	    console.log("onreadystatechange called: %O",xhr)
+	  },
+	  onload:function(xhr){
+	    console.log("onload called: %O",xhr)
+	  },
+	  //hook function
+	  open:function(arg,xhr){
+	    console.log("open called: method:%s,url:%s,async:%s",arg[0],arg[1],arg[2])
+	    
+	    var token_url = "/oauth/token";
     	var authoriz_url = "/oauth/authorize";
     	var client_id="client";
     	var secret="secret";
@@ -19,11 +57,9 @@ $.ajaxSetup({
         	 					+ "&client_id=" + client_id 
         	 					+ "&client_secret=" + secret 
         	 					+ "&grant_type=" + grant_type;
-        	 
          } else {
         	 var token = window.sessionStorage.getItem('access_token');
         	 xhr.setRequestHeader("Authorization", "Bearer " + token);
-        	 
-         }
-     }
+         } 
+	  }
 });
