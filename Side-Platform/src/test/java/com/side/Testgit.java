be86b206eb9ad1a95c3482661bc8da99dc856ceb
翁丹霞 <1024090566@qq.com>
2019-08-14 18:50:56 +0800

    测试git的提交

---
+++ Side-Platform/src/test/java/com/side/Testgit.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.side;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author gmc
+ *
+ */
+public class Testgit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
